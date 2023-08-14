0d4feb0f53e5518aac40c5b2f79927796a2ea7eb
Web File 8 <web.kelas8a@gmail.com>
2023-08-14 23:01:28 +0700

    10

--- main/src/js/script.js
+++ main/src/js/script.js
@@ -1,41 +1,27 @@
-$(document.querySelector).ready();
+const slider = document.querySelector('.list-x');
+let isDown = false;
+let startX;
+let scrollLeft;
 
-//Menu
-$(navbarNav = document.querySelector('.navbar-nav'));
-document.querySelector('#menu').
-onclick = (e) => {
-    navbarNav.classList.toggle('active');
-    e.preventDefault();
-    
-};
-
-
-//Search
-
-$(searchForm = document.querySelector('.search-form'));
-$(searchBox = document.querySelector('#search-box'));
-
-document.querySelector('#search-button').onclick = (e) => {
-    searchForm.classList.toggle('active');
-    searchBox.focus();
-    e.preventDefault();
-}
-
-
-//klik di luar element
-$(menu = document.querySelector('#menu'));
-$(sb = document.querySelector('#search-button'));
-
-document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
-
-//Search box
-
-document.addEventListener('click', function(e){
-    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
-        searchForm.classList.remove('active');
-    }
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
 });
